07c98ab12814d88654f88e7f89753144e8e8701c
Nina Weber <weber_nina@web.de>
2024-02-08 16:54:31 +0100

    add flex styles

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ const darkModeButton = document.querySelector('[data-js="dark-mode-button"]');
 darkModeButton.addEventListener("click", () => {
   bodyElement.classList.toggle("dark");
 });
+
+// Darken navbar sections:
+// const navbarButtons = document.querySelector('[data-js="navbar"]');
+
+// const navbarButtonHome = document.querySelector('[data-js="navbar-1"]');
+// navbarButtons.addEventListener("click", () => {
+//   navbarButtons.classList.add("navbar-highlight");
+// });
