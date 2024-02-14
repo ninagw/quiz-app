3b13830fcc593009b3c4173299fc29dfef6831c4
Nina Weber <weber_nina@web.de>
2024-02-14 17:04:32 +0100

    add bookmark function

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+const bookmarkButton = document.querySelector('[data-js="bookmark-button"]');
+const bookmarkIcon = document.querySelector('[data-js="bookmark-icon"]');
+
+// Bookmark Button switch to active:
+bookmarkIcon.addEventListener("click", () => {
+  bookmarkIcon.classList.toggle("bookmark-active");
+});
 
 // Switch Dark Mode Button On:
-const darkModeButton = document.querySelector('[data-js="dark-mode-button"]');
+// const darkModeButton = document.querySelector('[data-js="dark-mode-button"]');
 
-darkModeButton.addEventListener("click", () => {
-  bodyElement.classList.toggle("dark");
-});
+// darkModeButton.addEventListener("click", () => {
+//   bodyElement.classList.toggle("dark");
+// });
 
 // Darken navbar sections:
 // const navbarButtons = document.querySelector('[data-js="navbar"]');
