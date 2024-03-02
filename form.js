062a0b8f53dfd523b2c85f8b06a195652f9dade6
Nina Weber <weber_nina@web.de>
2024-03-02 13:49:48 +0100

    add character count

--- form.js
+++ form.js
@@ -6,7 +6,7 @@ const submitButton = document.querySelector('[data-js="submit-button"]');
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  //Read all entered data from the input fields:
+  // Read all entered data from the input fields:
   const question = event.target.elements.questionInput.value;
   const answer = event.target.elements.answerInput.value;
 
@@ -29,7 +29,7 @@ form.addEventListener("submit", (event) => {
   const newBookmarkIcon = document.createElement("span");
   newBookmarkIcon.classList.add("bookmark-empty");
 
-  // append card to form.html
+  // ------- append card to form.html ---------- //
   document.body.append(newQuestionCard);
   newQuestionCard.append(newQuestion);
   newQuestionCard.append(newAnswerButton);
@@ -42,44 +42,29 @@ form.addEventListener("submit", (event) => {
   event.target.questionInput.focus();
 });
 
-function characters() {
-  const questionInput = document.querySelector('[data-js="question-input"]');
-  const answerInput = document.querySelector('[data-js="answer-input"]');
+// --------- COUNT CHARACTERS LEFT ----- //
 
-  const charactersLeftQuestion = document.querySelector(
-    '[data-js="characters-left-question"]'
-  );
-  const charactersLeftAnswer = document.querySelector(
-    '[data-js="characters-left-answer"]'
-  );
+// --- characters of question input: --- //
+const questionInput = document.querySelector('[data-js="question-input"]');
+const charactersLeftQuestion = document.querySelector(
+  '[data-js="characters-left-question"]'
+);
 
-  const maxLengthQuestion = 200;
-  const maxLengthAnswer = 200;
+questionInput.addEventListener("input", () => {
+  const maxLengthQuestion = 150;
+  const remainingCharactersLeftQuestion =
+    maxLengthQuestion - questionInput.value.length;
+  charactersLeftQuestion.textContent = remainingCharactersLeftQuestion;
+});
 
-  // update characters of question input:
-  questionInput.addEventListener("input", () => {
-    charactersLeftQuestion(maxLengthQuestion - questionInput.value.length);
-    return;
-  });
-
-  const updateAmountLeftQuestion = (value) => {
-    charactersLeftQuestion.textContent = value;
-  };
-
-  updateAmountLeftQuestion(maxLengthQuestion);
-
-  // document.querySelector(
-  //   '[data-js="characters-left-question"]'
-  // ).textContent = `${maxLengthQuestion}`;
-
-  // update characters of answer input:
-  const updateAmountLeftAnswer = (value) => {
-    charactersLeftAnswer.innerText = value;
-  };
-
-  updateAmountLeftAnswer(maxLengthAnswer);
-
-  answerInput.addEventListener("input", () => {
-    charactersLeftAnswer(maxLengthAnswer - answerInput.value.length);
-  });
-}
+// --- characters of answer input: --- //
+const answerInput = document.querySelector('[data-js="answer-input"]');
+const charactersLeftAnswer = document.querySelector(
+  '[data-js="characters-left-answer"]'
+);
+answerInput.addEventListener("input", () => {
+  const maxLengthAnswer = 150;
+  const remainingCharactersLeftAnswer =
+    maxLengthAnswer - answerInput.value.length;
+  charactersLeftAnswer.textContent = remainingCharactersLeftAnswer;
+});
