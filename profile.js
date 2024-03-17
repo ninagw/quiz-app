734b296d26c9ed7307e01ad53a4b99be00e76e77
Nina Weber <weber_nina@web.de>
2024-03-17 11:37:51 +0100

    change profile.js

--- profile.js
+++ profile.js
@@ -1,10 +1,8 @@
 console.clear();
 
-function toggleDarkMode() {
-  const darkModeButton = document.querySelector('[data-js="dark-mode-button"]');
+const darkModeButton = document.querySelector('[data-js="dark-mode-button"]');
 
-  // Switch Dark Mode Button On/Off:
-  darkModeButton.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
-  });
-}
+// Switch Dark Mode Button On/Off:
+darkModeButton.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+});
