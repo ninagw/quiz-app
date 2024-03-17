66b8ea72ff56012408c40f10aef69bb84037a192
Nina Weber <weber_nina@web.de>
2024-03-17 12:01:20 +0100

    change index.js / add querySelectorAll to toggle all bookmarkIcons

--- index.js
+++ index.js
@@ -4,13 +4,23 @@ const bodyElement = document.querySelector('[data-js="body"]');
 const questionCard = document.querySelector('[data-js="question-card"]');
 const bookmarkButton = document.querySelector('[data-js="bookmark-button"]');
 const bookmarkIcon = document.querySelector('[data-js="bookmark-icon"]');
+const bookmarkIcons = document.querySelectorAll('[data-js="bookmark-icons"]');
 const answerButton = document.querySelector('[data-js="answer-button"]');
 const answer = document.querySelector('[data-js="answer"]');
 
 // Bookmark Button switch to active:
-bookmarkIcon.addEventListener("click", () => {
-  bookmarkIcon.classList.toggle("bookmark-active");
+bookmarkIcons.forEach((bookmarkIcon) => {
+  bookmarkIcon.addEventListener("click", () => {
+    bookmarkIcon.classList.toggle("bookmark-active");
+  });
 });
+
+// JANAS ANSATZ:
+// bookmark_buttons.forEach((bookmark_button) => {
+//   bookmark_button.addEventListener("click", () => {
+//     bookmark_button.classList.toggle("bookmarkChecked");
+//   });
+// });
 
 // Click answerButton and display/hide answer:
 answerButton.addEventListener("click", () => {
