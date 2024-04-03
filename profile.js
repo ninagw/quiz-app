c8a938232d8776e2a380e1aee075b088a00b58ec
Nina Weber <weber_nina@web.de>
2024-04-03 20:09:30 +0200

    change dark mode / profile.js

--- profile.js
+++ profile.js
@@ -5,4 +5,14 @@ const darkModeButton = document.querySelector('[data-js="dark-mode-button"]');
 // Switch Dark Mode Button On/Off:
 darkModeButton.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
+
+  // Save preference in local storage:
+  const isDarkMode = document.body.classList.contains("dark-mode");
+  localStorage.setItem("darkMode", isDarkMode);
 });
+
+// Check local storage if preference is dark mode:
+const isDarkMode = localStorage.getItem("darkMode") === true;
+if (isDarkMode) {
+  document.body.classList.add("dark-mode");
+}
