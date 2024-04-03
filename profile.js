ee18ed804db18ee3e4434cc796fb6bbd89f7feeb
Nina Weber <weber_nina@web.de>
2024-04-03 20:46:58 +0200

    implement dark mode on all pages, local storage

--- profile.js
+++ profile.js
@@ -1,27 +1,9 @@
-// console.clear();
-
-// const darkModeButton = document.querySelector('[data-js="dark-mode-button"]');
-
-// // Switch Dark Mode Button On/Off:
-// darkModeButton.addEventListener("click", () => {
-//   document.body.classList.toggle("dark-mode");
-
-//   // Save preference in local storage:
-//   const isDarkMode = document.body.classList.contains("dark-mode");
-//   localStorage.setItem("darkMode", isDarkMode);
-// });
-
-// // Check local storage if preference is dark mode:
-// const isDarkMode = localStorage.getItem("darkMode") === true;
-// if (isDarkMode) {
-//   document.body.classList.add("dark-mode");
-// }
+console.clear();
 
 const darkModeButton = document.querySelector('[data-js="dark-mode-button"]');
 
-// Function to toggle dark mode
+// Switch dark mode button on/off
 function toggleDarkMode() {
-  // Toggle dark mode class on body
   document.body.classList.toggle("dark-mode");
 
   // Save preference to local storage
